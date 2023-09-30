42a7266b434c0d9c15c56d4ccff8d0d8458866aa
rudra-t <rtiwari1308@gmail.com>
2023-09-30 11:45:06 +0530

    first commit

---
+++ website.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 200);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".nav-list");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.alert;
+};
